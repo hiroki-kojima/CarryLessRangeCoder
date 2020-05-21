207bcdbace0a333d7f3f5bf3d1e03c3c5a69b22a
hiroki-kojima <iamhirok@icloud.com>
2020-05-22 01:54:21 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+from os import path
 from setuptools import setup
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='CarryLess-RangeCoder',
+    name='carryless_rangecoder',
     version='1.0',
-    packages=['carryless_rangecoder']
+    url='https://github.com/hiroki-kojima/CarryLessRangeCoder',
+    author='hiroki-kojima',
+    author_email='hiroki.kojima.1997@gmail.com',
+    description='Python implementation of carry-less range coder',
+    packages=['carryless_rangecoder'],
+    long_description=long_description,
 )
